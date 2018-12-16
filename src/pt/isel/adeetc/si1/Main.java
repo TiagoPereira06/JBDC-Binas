e5993712ff7a8ce0305646b4fc70134748b7422c
Pedro Tereso <pedrotereso4@gmail.com>
2018-12-16 12:54:52 +0000

    test commit

--- src/pt/isel/adeetc/si1/Main.java
+++ src/pt/isel/adeetc/si1/Main.java
@@ -3,6 +3,7 @@ package pt.isel.adeetc.si1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("TESTE");
+        System.out.println("FDS PARA ESTA MERDA");
+        System.out.println("ESDRAS!");
     }
 }
