6b78959d142da54804b2791cdafaa237a9f26cc3
Tiago Pereira <tiagomrp06@gmail.com>
2018-12-16 13:09:13 +0000

    first commit

--- src/pt/isel/adeetc/si1/Main.java
+++ src/pt/isel/adeetc/si1/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("FDS PARA ESTA MERDA");
         System.out.println("ESDRAS!");
+        System.out.println("VW AUTOEUROPA!");
     }
 }
